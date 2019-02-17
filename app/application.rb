c098993de76a42913958b4377facba51c5af0f7d
Shemar Dacosta <dshemar6@gmail.com>
2019-02-17 04:29:21 +0000

    Automatically backed up by Learn

--- app/application.rb
+++ app/application.rb
@@ -4,6 +4,12 @@ class Application
   def call(env)
     resp = Rack::Response.new
      time = Time.now
+     
+     if time > 12
+     resp.write("Good Afternoon")
+   else 
+     
+   end
  
     resp.finish
   end
